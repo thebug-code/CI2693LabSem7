82b40e7bb2197842c9e8ca87b55ab1742bcc53ea
thebug-code <alejandrocardie12@gmail.com>
2022-07-11 21:48:17 -0400

    add toString()

--- libGrafoKt/ve/usb/libGrafo/VerticeBF.kt
+++ libGrafoKt/ve/usb/libGrafo/VerticeBF.kt
@@ -2,4 +2,6 @@ package ve.usb.libGrafo
 
 import kotlin.Double.Companion.POSITIVE_INFINITY as inf
 
-public class VerticeBF(var v: Int, var d: Double = inf, var pred: ArcoCosto? = null) 
+public class VerticeBF(var v: Int, var d: Double = inf, var pred: ArcoCosto? = null) {
+    override fun toString(): String = "($v, $d, ${pred})"
+}
